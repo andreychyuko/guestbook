fb1cbb4493d56e9ec4b3a55a4da415ed1a131335
andreychyuko <andreychyuko@gmail.com>
2020-08-16 13:22:35 +0300

    Chuyko:Fix

--- apps/news/forms.py
+++ apps/news/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import News
+import re # импорт регулярных выражений 
+from django.core.exceptions import ValidationError # импорт валидатора обработки ошибок
+
 
 class NewsForm(forms.ModelForm):
     class Meta:
@@ -10,3 +13,14 @@ class NewsForm(forms.ModelForm):
             'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
             'category': forms.Select(attrs={'class': 'form-control'}),
         }
+
+    #валидатор для названия что бы не начиналось с цифры
+    def clean_title(self):
+        # получаем очищенные данные славоря по ключу title
+         title = self.cleaned_data['title']
+          # условие не начинается ли эта строка с цифры
+         if re.match(r'\d', title):
+             # если начинается то выходит исключение 
+          raise ValidationError('Название не должно начинаться с цифры')
+            # если мы прошли данную проверку тогда мы возвращаем title
+         return title
