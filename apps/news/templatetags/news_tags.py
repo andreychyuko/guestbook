0baa1fc946ac2a6d52b8b36457005214e1891550
andreychyuko <andreychyuko@gmail.com>
2020-08-27 12:55:45 +0300

    Chuyko:Fix

--- apps/news/templatetags/news_tags.py
+++ apps/news/templatetags/news_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Count
 
 from news.models import Category
 
@@ -11,7 +12,8 @@ def get_categories():
 
 @register.inclusion_tag('news/list_categories.html')
 def show_categories(arg1='Hello', arg2='Imper'):
-    categories=Category.objects.all()
+    #categories=Category.objects.all()
+    categories = Category.objects.annotate(cnt=Count('news')).filter(cnt__gt=0)
     return {"categories" : categories, "arg1":arg1,"arg2":arg2}
 
 @register.filter
